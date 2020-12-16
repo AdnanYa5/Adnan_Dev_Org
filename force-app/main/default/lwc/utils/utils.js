afd387819dda229543f1ac1a97ea3fe5cdbb16c7
AdnanYa5 <67212631+AdnanYa5@users.noreply.github.com>
2020-12-17 01:51:44 +1100

    indentation commit

--- force-app/main/default/lwc/utils/utils.js
+++ force-app/main/default/lwc/utils/utils.js
@@ -1,38 +1,38 @@
-export function exportCSVFile(headers, totalData, fileTitle){
-    if(!totalData || !totalData.length){
+export function exportCSVFile(headers, totalData, fileTitle) {
+    if (!totalData || !totalData.length) {
         return null
     }
 
     const jsonObject = JSON.stringify(totalData);
     const result = convertToCSV(jsonObject, headers);
-    if(result === null) return;
+    if (result === null) return;
     const blob = new Blob([result]);
-    const exportedFileName = fileTitle ? fileTitle+'.csv' : 'export.csv';
-    if(navigator.msSaveBlob){
+    const exportedFileName = fileTitle ? fileTitle + '.csv' : 'export.csv';
+    if (navigator.msSaveBlob) {
         navigator.msSaveBlob(blob, exportedFileName);
-    } else if(navigator.userAgent.match(/iPhone|iPad|iPod/i)){
+    } else if (navigator.userAgent.match(/iPhone|iPad|iPod/i)) {
         const link = window.document.createElement('a');
-        link.href='data:text/csv;charset-8,' + encodeURI(result);
-        link.target="_blank";
-        link.download=exportedFileName;
+        link.href = 'data:text/csv;charset-8,' + encodeURI(result);
+        link.target = "_blank";
+        link.download = exportedFileName;
         link.click();
     } else {
         const link = document.createElement("a");
-    if(link.download !== undefined){
-        const url = URL.createObjectURL(blob);
-        link.setAttribute("href", url);
-        link.setAttribute("download", exportedFileName);
-        link.style.visibility = "hidden";
-        document.body.appendChild(link);
-        link.click();
-        document.body.removeChild(link);
-    }
+        if (link.download !== undefined) {
+            const url = URL.createObjectURL(blob);
+            link.setAttribute("href", url);
+            link.setAttribute("download", exportedFileName);
+            link.style.visibility = "hidden";
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        }
     }
 
-    
+
 }
 
-function convertToCSV(objArray, headers){
+function convertToCSV(objArray, headers) {
     const columnDelimiter = ',';
     const lineDelimiter = '\r\n';
     const actualHeaderKey = Object.keys(headers);
@@ -42,19 +42,19 @@ function convertToCSV(objArray, headers){
     str += headerToShow.join(columnDelimiter);
     str += lineDelimiter;
     const data = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
-    
+
     data.forEach(obj => {
         let line = '';
-        actualHeaderKey.forEach(key=>{
-            if(line != ''){
-                line+=columnDelimiter;
+        actualHeaderKey.forEach(key => {
+            if (line != '') {
+                line += columnDelimiter;
             }
-            let strItem = obj[key]+'';
-            line+=strItem? strItem.replace(/,/g, ''):strItem;
+            let strItem = obj[key] + '';
+            line += strItem ? strItem.replace(/,/g, '') : strItem;
         });
-        str+=line+lineDelimiter;
+        str += line + lineDelimiter;
     });
-    console.log("str--"+str);
+    console.log("str--" + str);
     return str;
 
 }
